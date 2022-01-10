dd2ba41612d3abd42a8654f339a68968f7e4cdec
lectroMathew <contact@lectro.moe>
2022-01-10 03:24:45 +0300

    Added top bar, checkboxes for enabling panels

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,8 @@
 use eframe::{egui, epi};
-use eframe::egui::{CentralPanel, Color32, Pos2, Vec2, Window};
+use eframe::egui::{CentralPanel, Color32, CtxRef, Pos2, Vec2, Window};
+use eframe::egui::Event::Key;
+use eframe::egui::Key::A;
+use eframe::epi::Frame;
 
 pub struct ApplicationConfig {
     pub side_enabled: (bool, bool),
@@ -19,6 +22,7 @@ impl ApplicationConfig {
 pub struct App {
     pub config: ApplicationConfig,
     exit_requested: bool,
+    resize_requested: bool,
 }
 
 impl Default for App {
@@ -26,41 +30,98 @@ impl Default for App {
         Self {
             config: ApplicationConfig::new(),
             exit_requested: false,
+            resize_requested: false,
         }
     }
 }
 
 impl App {
-    pub(crate) fn exit(&mut self) {
+    pub fn exit(&mut self) {
         self.exit_requested = true;
+    }
+
+    fn recalculate_size(&mut self, frame: &Frame) {
+        let mut new_size = Vec2::new(500., 800.);
+
+        if self.config.side_enabled.0 && self.config.side_enabled.1 {
+            new_size.x += 500.0;
+        }
+
+        frame.set_window_size(new_size);
+    }
+
+    fn draw_right_panel(&mut self, ctx: &CtxRef) {
+        egui::SidePanel::right("right_panel")
+            .resizable(false)
+            .min_width(500.0)
+            .max_width(500.0)
+            .show(ctx, |ui| {
+                // egui::Area::new("right_words_window")
+                //     .fixed_pos(Pos2::new(10., 10.))
+                //     .show(ctx, |ui| {
+                //         ui.label("Right Panel!");
+                //         ui.label("Right Panel!");
+                //         ui.label("Right Panel!");
+                //         ui.label("Right Panel!");
+                //         ui.label("Right Panel!");
+                //     })
+            });
+    }
+
+    fn draw_left_panel(&mut self, ctx: &CtxRef) {
+        // using CentralPanel because SidePanel::left adds additional width and doesn't end in the center
+        egui::CentralPanel::default()
+            .show(ctx, |ui| {
+                ui.label("HI");
+        });
+    }
+
+    fn draw_about_window(ctx: &CtxRef) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Built with egui");
+            ui.label("lectroMathew, 2022");
+        });
+    }
+
+    fn draw_top_bar(&mut self, ctx: &CtxRef) {
+        egui::TopBottomPanel::top("top_bar").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.checkbox(&mut self.config.side_enabled.0, "Left panel").changed() {
+                    self.resize_requested = true;
+                }
+                if ui.checkbox(&mut self.config.side_enabled.1, "Right panel").changed() {
+                    self.resize_requested = true;
+                }
+            });
+        });
     }
 }
 
 impl epi::App for App {
     fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
 
+        self.draw_top_bar(ctx);
+
         match &mut self.config.side_enabled {
-            (false, false) => {},
-            (false, true) => {},
-            (true, false) => {},
-            (true, true) => {},
+            (false, false) => {
+                App::draw_about_window(ctx);
+            },
+            (false, true) => {
+                self.draw_right_panel(ctx);
+            },
+            (true, false) => {
+                self.draw_left_panel(ctx);
+            },
+            (true, true) => {
+                self.draw_right_panel(ctx);
+                self.draw_left_panel(ctx);
+            },
         }
 
-        egui::SidePanel::right("my_side_panel")
-            .resizable(false)
-            .min_width(500.0)
-            .show(ctx, |ui| {
-                ui.label("Right Panel!");
-                ui.label("Right Panel!");
-                ui.label("Right Panel!");
-                ui.label("Right Panel!");
-                ui.label("Right Panel!");
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("HI");
-        });
-
+        if self.resize_requested {
+            self.recalculate_size(frame);
+            self.resize_requested = false;
+        }
 
         if self.exit_requested {
             frame.quit()
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ use eframe::egui::Vec2;
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     let app = HemiTyper::App::default();
-    let mut native_options = eframe::NativeOptions::default();
-    native_options.initial_window_size = Some(Vec2::new(1000.0, 800.0));
+
+    let mut native_options = eframe::NativeOptions {
+        initial_window_size: Some(Vec2::new(1000.0, 800.0)),
+        resizable: false,
+        ..Default::default()
+    };
 
     eframe::run_native(Box::new(app), native_options);
 }
