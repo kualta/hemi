924ef7b180bed0002a0e27682d098d42d2c68b05
lectroMathew <contact@lectro.moe>
2022-09-17 08:46:20 +0300

    added info panel, small changes

--- hemi_typer.js
+++ hemi_typer.js
@@ -1028,20 +1028,20 @@ imports.wbg.__wbindgen_memory = function() {
     const ret = wasm.memory;
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper500 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 192, __wbg_adapter_26);
+imports.wbg.__wbindgen_closure_wrapper509 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 199, __wbg_adapter_26);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper502 = function(arg0, arg1, arg2) {
-    const ret = makeClosure(arg0, arg1, 192, __wbg_adapter_29);
+imports.wbg.__wbindgen_closure_wrapper511 = function(arg0, arg1, arg2) {
+    const ret = makeClosure(arg0, arg1, 199, __wbg_adapter_29);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper676 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 313, __wbg_adapter_32);
+imports.wbg.__wbindgen_closure_wrapper685 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 320, __wbg_adapter_32);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper742 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 343, __wbg_adapter_35);
+imports.wbg.__wbindgen_closure_wrapper751 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 350, __wbg_adapter_35);
     return addHeapObject(ret);
 };
 
